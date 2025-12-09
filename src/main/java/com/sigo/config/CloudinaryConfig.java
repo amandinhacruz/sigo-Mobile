0d9d1895a602afdbb7517aa50e6e274f38f3d141
amandinhacruz <amandaellem2023@gmail.com>
2025-12-08 22:59:18 -0300

    m' m
    '

--- src/main/java/com/sigo/config/CloudinaryConfig.java
+++ src/main/java/com/sigo/config/CloudinaryConfig.java
@@ -1,16 +1,32 @@
 package com.sigo.config;
 
+
+
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
 
+@Configuration
 public class CloudinaryConfig {
+
+    @Value("${dmvughncq}")
+    private String cloudName;
+
+    @Value("${275324558262478}")
+    private String apiKey;
+
+    @Value("${A2rA1Jkn5UcKpdhn2vIB1t8BptM}")
+    private String apiSecret;
+
     @Bean
     public Cloudinary cloudinary() {
         return new Cloudinary(ObjectUtils.asMap(
-                "cloud_name", "SEU_CLOUD_NAME",
-                "api_key", "SUA_API_KEY",
-                "api_secret", "SEU_API_SECRET"
+                "cloud_name", cloudName,
+                "api_key", apiKey,
+                "api_secret", apiSecret
         ));
     }
 }
+
